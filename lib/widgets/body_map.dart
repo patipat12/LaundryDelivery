8f8a4018a4281832b1e589d23cde14edec7a3dbc
fewfew <few@gmail.com>
2024-07-28 14:12:49 +0700

    googlemap success

--- lib/widgets/body_map.dart
+++ lib/widgets/body_map.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:testdb/utility/app_controller.dart';
+import 'package:testdb/utility/app_service.dart';
 
-class BodyMap extends StatelessWidget {
+class BodyMap extends StatefulWidget {
   const BodyMap({super.key});
 
   @override
+  State<BodyMap> createState() => _BodyMapState();
+}
+
+class _BodyMapState extends State<BodyMap> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    AppServicr().processFinPosition();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Text('This is map');
+    return GetX<AppController>(
+      init: AppController(),
+      initState: (_) {},
+      builder: (AppController appController) {
+        return appController.positions.isEmpty
+            ? const SizedBox()
+            : GoogleMap(
+                initialCameraPosition: CameraPosition(
+                    target: LatLng(appController.positions.last.latitude, appController.positions.last.longitude), zoom: 16));
+      },
+    );
   }
 }
