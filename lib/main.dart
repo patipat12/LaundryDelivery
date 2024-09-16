9cecca69f6367d901e2aad3aace9cbe81d04ef33
fewfew <few@gmail.com>
2024-09-16 13:07:43 +0700

    update firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:testdb/firebase_options.dart';
 import 'package:testdb/screens/login_page.dart';
 import 'package:testdb/screens/main_home.dart';
 import 'package:testdb/utility/app_http_override.dart';
 
 List<GetPage<dynamic>>? getPages = [
-  GetPage(name: '/loginPage', page: () => const LoginPage(),),
-  GetPage(name: '/mainHome', page: () => const MainHome(),),
+  GetPage(
+    name: '/loginPage',
+    page: () => const LoginPage(),
+  ),
+  GetPage(
+    name: '/mainHome',
+    page: () => const MainHome(),
+  ),
 ];
 
 String? firstPage;
@@ -18,34 +26,22 @@ Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  await GetStorage.init().then(
-    (value) {
-      var result = GetStorage().read('data');
-      print('## result ===> $result');
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then((value) async {
+    await GetStorage.init().then(
+      (value) {
+        var result = GetStorage().read('data');
+        print('## result ===> $result');
 
-      if (result == null) {
-
-        firstPage = '/loginPage';
-        runApp(const MyApp());
-        
-      } else {
-
-        firstPage = '/mainHome';
-           runApp(const MyApp());
-        
-        
-      }
-
-
-
-
-
-
-
-    },
-  );
-
-
+        if (result == null) {
+          firstPage = '/loginPage';
+          runApp(const MyApp());
+        } else {
+          firstPage = '/mainHome';
+          runApp(const MyApp());
+        }
+      },
+    );
+  });
 }
 
 class MyApp extends StatelessWidget {
